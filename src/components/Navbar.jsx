b9fd9e65ffc05e9676235dbf0af94e21c1e9eddb
Gabriel Caramés <49535753+GabrielCarames@users.noreply.github.com>
2022-06-24 20:52:44 -0300

    react wordle

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar({finishedGame, setFinishedGame, setInstructions})
     return (
         <nav className="navbar">
             <img className="navbar__logo" src={helpLogo} alt="Ayuda" onClick={() => setInstructions(true)} />
-            <h1 className="navbar__title">WORDLE CLONE</h1>
+            <h1 className="navbar__title">REACT WORDLE</h1>
             <img className="navbar__logo" src={statisticsLogo} alt="Estadísticas" onClick={() => setFinishedGame({...finishedGame, state: true})} />
         </nav>
     )
